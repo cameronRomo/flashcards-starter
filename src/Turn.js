c2e0f77ad5993376e5f9c6bae141827e23ea231c
Cameron Romo <cromo1225@gmail.com>
2020-10-06 20:47:27 -0600

    Add methods returnGuess(), evaluateGuess(), and giveFeedbackk() and a property for is true

--- src/Turn.js
+++ src/Turn.js
@@ -2,6 +2,31 @@ class Turn {
   constructor(prediction, card) {
     this.guess = prediction;
     this.currentCard = card;
+    this.isCorrect = false;
+  }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.currentCard;
+  }
+
+  evaluateGuess() {
+    if (this.guess !== this.currentCard.correctAnswer) {
+      return this.isCorrect = false;
+    } else {
+      return this.isCorrect = true;
+    }
+  }
+
+  giveFeeback() {
+    if (this.isCorrect === true) {
+      return 'Correct!'
+    } else {
+      return 'Incorrect!'
+    }
   }
 }
 
