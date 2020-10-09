115df59828a8827ca70f7069308247f0ca5bf9da
Cameron Romo <cromo1225@gmail.com>
2020-10-08 20:00:36 -0600

    Add start() to run the necessary classes and printMessage()/printQuestion()

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,22 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('../src/Deck.js');
+const Round = require('../src/Round.js');
+const Card = require('../src/Card.js');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.round = null;
+  }
+
+  start() {
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.round = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
